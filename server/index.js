d66edf2365eb78322f1f65e5cb76236466729a83
hyunjin-C <zn9852@naver.com>
2023-09-09 03:23:40 +0900

    서버 index.js에 오타 수정

--- server/index.js
+++ server/index.js
@@ -46,32 +46,32 @@ app.post("/api/users/register", (req, res) => {
 // 로그인
 app.post("/api/users/login", (req, res) => {
   // 요청된 이메일을 데이터베이스에 있는지 찾는다.
-  User.findOne({ email: req.body.email }).then(() => {
+  User.findOne({ email: req.body.email }).then((err, user) => {
     if (!user) {
       return res.json({
         loginSuccess: false,
         message: "제공된 이메일에 해당하는 유저가 없습니다.",
       });
     }
-  });
 
-  // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는지 확인한다.
-  user.comparePassword(req.body.password, (err, isMatch) => {
-    if (!isMatch) {
-      return res.json({
-        loginSuccess: false,
-        message: "비밀번호가 틀렸습니다.",
+    // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는지 확인한다.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+      }
+      // 비밀번호가 맞다면 토큰 생성한다.
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지, 세션스토리지 등 다양한 곳에 저장 가능 -> 모두 장단점이 있음
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
       });
-    }
-    // 비밀번호가 맞다면 토큰 생성한다.
-    user.generateToken((err, user) => {
-      if (err) return res.status(400).send(err);
-
-      // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지, 세션스토리지 등 다양한 곳에 저장 가능 -> 모두 장단점이 있음
-      res
-        .cookie("x_auth", user.token)
-        .status(200)
-        .json({ loginSuccess: true, userId: user._id });
     });
   });
 });
