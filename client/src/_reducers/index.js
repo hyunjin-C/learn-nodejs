3611641228b15fb9fb7bbaf61e041ffedde4297c
hyunjin-C <zn9852@naver.com>
2023-09-08 14:45:39 +0900

    Redux 셋팅

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+// import user from "./user_reducer";
+
+// 여러 개의 reducer를 combineReducer를 통해 rootReducer로 합쳐줌
+const rootReducer = combineReducers({
+  // user,
+});
+
+export default rootReducer;
--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,30 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import { Provider } from "react-redux"; // redux와 app 연결
+import { applyMiddleware, createStore } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Reducer from "./_reducers";
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider
+        store={createStoreWithMiddleware(
+          Reducer,
+          window.__REDUX_DEVTOOLS_EXTENSION__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+        )}
+      >
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
