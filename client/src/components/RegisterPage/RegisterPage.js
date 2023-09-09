58f0ced64314b492a44c35bb3de17c84eccfc32e
hyunjin-C <zn9852@naver.com>
2023-09-09 13:11:28 +0900

    Redux를 사용한 회원가입 페이지 구현

--- client/src/components/RegisterPage/RegisterPage.js
+++ client/src/components/RegisterPage/RegisterPage.js
@@ -1,7 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { RegisterUser } from "../../_actions/user_action";
 
-function Register() {
-  return <div>Register</div>;
+function RegisterPage(props) {
+  const dispatch = useDispatch();
+
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const [Name, setName] = useState("");
+  const [ConfirmPassword, setConfirmPassword] = useState("");
+
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+
+  const onNameHandler = (event) => {
+    setName(event.currentTarget.value);
+  };
+
+  const onConfirmPasswordHandler = (event) => {
+    setConfirmPassword(event.currentTarget.value);
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log("Name", Name);
+    console.log("Email", Email);
+    console.log("PW", Password);
+
+    if (Password !== ConfirmPassword) {
+      return alert("비밀번호와 비밀번호 확인은 같아야 합니다.");
+    }
+
+    let body = {
+      name: Name,
+      email: Email,
+      Password: Password,
+    };
+
+    dispatch(RegisterUser(body)).then((response) => {
+      if (response.payload.success) {
+        props.history.push("/login");
+      } else {
+        alert("Failed to sign up");
+      }
+    });
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form style={{ display: "flex", flexDirection: "column" }}>
+        <label>Name</label>
+        <input type="text" value={Name} onChange={onNameHandler} />
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label>Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          value={ConfirmPassword}
+          onChange={onConfirmPasswordHandler}
+        />
+        <br />
+
+        <button onClick={onSubmitHandler}>Login</button>
+      </form>
+    </div>
+  );
 }
 
-export default Register;
+export default RegisterPage;
